6a05764139fa4a78ea8dbe31b12ab517911b60b9
Jan Buschtöns <jan@buschtoens.me>
2018-01-25 15:19:42 +0100

    stash

--- server.mjs
+++ server.mjs
@@ -1,10 +1,12 @@
-import util from 'util';
 import envProxyAgent from 'env-proxy-agent';
 import fs from 'mz/fs';
+import jose from 'node-jose';
 import ACMEV2Client from './';
 import directoryUrls from './directory-urls';
+import AccountDoesNotExistError from './lib/errors/account-does-not-exist';
+import generateKey from './lib/generate-key';
 
-// const debug = util.debuglog('acme-server');
+const { JWK } = jose;
 
 const ACCOUNT_KEY_FILE = './account-key.json';
 
@@ -17,20 +19,52 @@ const directoryUrl = directoryUrls['letsencrypt-staging'];
     directory: await ACMEV2Client.loadDirectory(directoryUrl)
   });
 
+  let account;
   if (await fs.exists(ACCOUNT_KEY_FILE)) {
-    await acme.importAccountKey(await fs.readFile(ACCOUNT_KEY_FILE));
+    const key = await JWK.asKey(await fs.readFile(ACCOUNT_KEY_FILE));
+    try {
+      // try to find an existing account for that key
+      account = await acme.findAccountByKey(key);
+      console.log(`Found an account for ${key.kid}: ${account.accountURL}`);
+    } catch (error) {
+      // throw any other error than AccountDoesNotExistError
+      if (!(error instanceof AccountDoesNotExistError)) throw error;
+
+      // the account does not exist, so create one
+      console.log(`There's no account for ${key.kid}. Creating a new one.`);
+      account = await acme.createAccount({
+        key,
+        contact: ['mailto:j.buschtoens@ddg-online.de'],
+        termsOfServiceAgreed: true
+      });
+    }
   } else {
-    const accountKey = await acme.importAccountKey(
-      await ACMEV2Client.generateAccountKey()
+    // there's no key yet, so create a new account with an implicitly generated key
+    console.log(
+      `There's no key located at ${ACCOUNT_KEY_FILE}. Creating a new account.`
     );
+    account = await acme.createAccount({
+      key: await generateKey('RSA'),
+      contact: ['mailto:j.buschtoens@ddg-online.de'],
+      termsOfServiceAgreed: true
+    });
+    console.log(`Created a new account: ${account.accountURL}`);
+
+    // save the implicitly generated key
     await fs.writeFile(
       ACCOUNT_KEY_FILE,
-      JSON.stringify(accountKey.toJSON(true), null, 2)
+      JSON.stringify(account.key.toJSON(true), null, 2)
     );
+    console.log(`Saving key ${account.key.kid} as ${ACCOUNT_KEY_FILE}.`);
   }
 
-  const account = await acme.newAccount({
-    contact: ['j.buschtoens@ddg-online.de'],
-    termsOfServiceAgreed: true
+  // create a new order
+  const order = await account.createOrder({
+    identifiers: [
+      { type: 'dns', value: 'barmer.ddg-webservice.de' },
+      { type: 'dns', value: '*.barmer.ddg-webservice.de' }
+    ]
   });
+
+  console.log(order);
 })();
