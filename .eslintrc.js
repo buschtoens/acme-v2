937f56fdecd2f4da8efdfcce734a3037d291b3aa
Jan Buschtöns <jan@buschtoens.me>
2018-02-28 09:52:57 +0100

    chore(eslint): configure linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,8 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2017
   },
-  plugins: ['prettier'],
-  extends: ['eslint-config-airbnb-base', 'prettier'],
+  plugins: ['node', 'prettier'],
+  extends: ['eslint-config-airbnb-base', 'plugin:node/recommended', 'prettier'],
   rules: {
     'prettier/prettier': ['error', { singleQuote: true }],
     'no-param-reassign': ['error', { props: false }],
@@ -12,6 +12,8 @@ module.exports = {
     'no-restricted-syntax': 'off',
     'no-await-in-loop': 'off',
     'import/no-unresolved': 'off',
-    'import/extensions': 'off'
+    'import/no-extraneous-dependencies': 'off',
+    'import/extensions': 'off',
+    'node/no-unsupported-features': ['error', { ignores: ['modules'] }]
   }
 };
